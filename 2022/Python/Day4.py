534a56cf6b345197456a5446298892c36fcf1a15
Kshitij Dubey <87259045+KDubZz@users.noreply.github.com>
2022-12-04 21:22:32 +0000

    Prep for Day 5 + cleanup of Day 4

--- 2022/Python/Day4.py
+++ 2022/Python/Day4.py
@@ -9,16 +9,23 @@ def part_1():
 		h = value.split(',')[1]
 		s1, s2 = s.split('-')
 		h1, h2 = h.split('-')
-		"""
 		if int(s1) >= int(h1) and int(s2) <= int(h2):
 			count += 1
 		elif int(s1) <= int(h1) and int(s2) >= int(h2):
 			count += 1
-		"""
-		if int(s1) <= int(h2):
-			if int(h1) <= int(s2):
-				count += 1
+	return count
+
+
+def part_2():
+	count = 0
+	for value in inp:
+		s = value.split(',')[0]
+		h = value.split(',')[1]
+		s1, s2 = s.split('-')
+		h1, h2 = h.split('-')
+		if int(s1) <= int(h2) and int(h1) <= int(s2):
+			count += 1
 	return count
 
 if __name__ == '__main__':
-	print('Part 1:', part_1())
+	print('Part 1:', part_1(), '\nPart 2:', part_2())
---
+++ 2022/Python/Day5.py
@@ -0,0 +1,9 @@
+import aoc_utils
+
+def part_1():
+	inp = aoc_utils.input_
+
+def part_2():
+	inp = aoc_utils.input_
+
+print('Part 1:', part_1(), '\nPart 2:', part_2())
